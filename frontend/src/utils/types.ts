57f3cff3928218727220ae6dd45b1da95c012079
rahul <rahulkarki0608@gmail.com>
2026-04-12 20:00:03 +0530

    changes made

--- frontend/src/utils/types.ts
+++ frontend/src/utils/types.ts
@@ -1,6 +1,6 @@
 export interface ImportMetaEnv {
   readonly VITE_GOOGLE_CLIENT_ID: string;
-  readonly VITE_API_URL: string;
+  readonly VITE_API_BASE_URL: string;
 }
 
 export interface ImportMeta {
