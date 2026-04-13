93006f3b9180225f7a38ee715615d066e4eb497e
rahul <rahulkarki0608@gmail.com>
2026-04-13 23:21:33 +0530

    final chnages in google-login

--- frontend/src/components/ui/GoogleLoginButton.tsx
+++ frontend/src/components/ui/GoogleLoginButton.tsx
@@ -7,28 +7,35 @@ interface GoogleAuthButtonProps {
 
 const GoogleAuthButton = ({ redirectTo = "/resumes" }: GoogleAuthButtonProps) => {
   return (
-    <GoogleLogin
-      shape="pill"
-      onSuccess={async (credentialResponse) => {
-        try {
-          const res = await api.post("/auth/google-login", {
-            token: credentialResponse.credential,
-          });
+    <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+      <div style={{ width: "100%", maxWidth: "100%" }}>
+        <GoogleLogin
+          text="signin"
+          size="large"
+          shape="rectangular"
+          logo_alignment="center"
+          onSuccess={async (credentialResponse) => {
+            try {
+              const res = await api.post("/auth/google-login", {
+                token: credentialResponse.credential,
+              });
 
-          if (res.data?.accessToken) {
-            localStorage.setItem("accessToken", res.data.accessToken);
-          }
+              if (res.data?.accessToken) {
+                localStorage.setItem("accessToken", res.data.accessToken);
+              }
 
-          window.location.href = redirectTo;
-        } catch (err:any) {
-          alert(err);
-          console.error(err);
-        }
-      }}
-      onError={() => {
-        console.log("Login Failed");
-      }}
-    />
+              window.location.href = redirectTo;
+            } catch (err:any) {
+              alert(err);
+              console.error(err);
+            }
+          }}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
+      </div>
+    </div>
   );
 };
 
